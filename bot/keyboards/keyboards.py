50bb5f3d417e04fbd854d8932ca90717512c3579
Ivylou <princequincyy@gmail.com>
2022-10-20 01:15:40 +0300

    first commit

---
+++ bot/keyboards/keyboards.py
@@ -0,0 +1,12 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+info = ReplyKeyboardMarkup(resize_keyboard=True)
+i = KeyboardButton('info')
+info.add(i)
+
+send = InlineKeyboardMarkup(resize_keyboard=True)
+s = InlineKeyboardButton('send', callback_data='send')
+send.add(s)
+
+
